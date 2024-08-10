8290e7e5f91eeaf9982b40c2796d79d0fea7d78b
Raghavarshini Ganesan <varshaganesh98@gmail.com>
2024-08-11 01:41:49 +0800

    commit

--- src/Certifications.js
+++ src/Certifications.js
@@ -1,28 +1,42 @@
 // Certifications.js
-import React from 'react';
+
+import React, { useState } from 'react';
 import { PiCertificateDuotone } from 'react-icons/pi';
 import './Certifications.css';
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 
 const certifications = [
-  { title: 'Data Analytics & Visualization with Power BI', company: 'Skillsfuture-Intellisoft Systems' },
-  { title: 'Microsoft Certified: Azure Fundamentals', company: 'Microsoft' },
-  { title: 'Social and Behavioral Research', company: 'CITI Program' },
+  { title: 'Social and Behavioral Research', company: 'CITI Program', link: 'https://www.citiprogram.org/verify/?w67a36dc5-1538-4e03-bd92-37098d0d5fd7-53860032'},
+  { title: 'Data Analytics & Visualization with Power BI', company: 'Skillsfuture-Intellisoft Systems',link:'https://www.dropbox.com/scl/fi/tj37shgb0j0jyromvm6ts/Dataanalytics-skillsfuture.pdf?rlkey=y8oshmisgkqsapww2u054q0xg&st=7d8hyy9n&dl=0' },
+  { title: 'Microsoft Certified: Azure Fundamentals', company: 'Microsoft' , link:'https://www.credly.com/badges/1ef7d438-5d47-4639-8b8b-f9971a9af594/linked_in_profile'},
   
-  { title: 'Python Data Structures', company: 'Coursera' },
-  { title: 'Programming for Everybody', company: 'University of Michigan' },
-  { title: 'Cloud Computing', company: 'NPTEL IIT Kharagpur' },
+  { title: 'Python Data Structures', company: 'Coursera' , link:'https://www.coursera.org/account/accomplishments/certificate/5N84YARW36K2'},
+  { title: 'Programming using Python', company: 'University of Michigan', link:'https://www.coursera.org/account/accomplishments/verify/UDF7FLZV5PFV?utm_source=link&utm_medium=certificate&utm_content=cert_image&utm_campaign=sharing_cta&utm_product=course'},
+  { title: 'Cloud Computing', company: 'NPTEL IIT Kharagpur',link:'https://www.dropbox.com/scl/fi/5acbcgcfidi21qthnd96y/CLOUD-COMPUTING_NPTEL-ONLINE-CERTIFICATE_AUG-SEP-2018_01.jpg?rlkey=py4v11uhi5dznt430auohax9d&st=t9mltesj&dl=0' },
+
+  { title: 'Industrial Training Network & Cybersecurity', company: 'BSNL', link:'https://www.dropbox.com/scl/fi/91l77zboomfqmd621fe39/BSNL-CERTIFICATE-_-NETWORKING-AND-CYBER-SECURITY_280518TO150618.jpg?rlkey=615n4l0s90nij5vlaty79r3sp&st=kkgymwvl&dl=0' },
+  { title: 'Java Development', company: 'Uniq Technologies' , link:'https://www.dropbox.com/scl/fi/n83nlpx112x3oui72dwoz/CERTIFICTE-OF-COMPLETN_JAVA_MINI-PROJECT_UNIQ.jpg?rlkey=tvn581sul5r6e1iwjb4jcknuv&st=amvxg7ud&dl=0'},
+  { title: 'Inplant Training', company: 'Madras Atomic Power Station' ,link:'https://www.dropbox.com/scl/fi/wf77w8ov1d1woi54e5m8d/maps-internship-2019-12-23-07.49.07.pdf?rlkey=y79f17z8503jukkm3jrbmg839&st=3x9m3css&dl=0' },
+  { title: 'Devops Professional', company: 'Infosys',link:'https://www.dropbox.com/scl/fi/t9sr2s1nmwy7uskkhg6rj/Experience-certificate-Raghavarshini-G_1103102_28496.pdf?rlkey=rycvu1gbln4b4qwuu5yheiu2r&st=fck193n1&dl=0' }
 ];
 
 const Certifications = () => {
+
+
   return (
     <div className="certifications">
       {certifications.map((cert, index) => (
-        <div className="cert-card" key={index}>
+        <div className="cert-card" key={index} >
           <PiCertificateDuotone className="cert-icon" />
           <h3>{cert.title}</h3>
-          <p>{cert.company}</p>
+          
+          <a href={cert.link} target="_blank" rel="noopener noreferrer"><p>{cert.company}<br></br><FaExternalLinkAlt/></p></a>
         </div>
       ))}
+
+      
+
     </div>
   );
 };
--- src/HomePage.js
+++ src/HomePage.js
@@ -79,6 +79,10 @@ const HomePage = () => {
         </div> 
         
         <div className="aboutsection">
+        
+        <h2>MY CERTIFICATIONS:</h2>
+        
+        <div><Certifications/></div>
         <h2> ABOUT ME: </h2>
         
         <p>  With over 4 years of my expertise in the tech industry, I've had the opportunity to work on a variety of challenging projects across multiple domains where I've honed my skills and delivered impactful solutions. </p>
@@ -88,10 +92,17 @@ const HomePage = () => {
         <p> With my never ending thirst to adapt to recent trends and technologies I had moved to Singapore to Pursue my masters Degree in Information Systems at NTU. The overall experience was exciting and exposed me to newer domain of Data Analytics. I also worked part time as a Software Research Intern in an Ongoing Sustainability Project.</p>
         <><TbArrowBigDownLines/></>
         <p> After completing this milestone, I started working in Singapore as a Software Engineer. It has ben over an year & I am so grateful for the learnings and opportunities I have been exposed to in my career path and hope that I will be on track with the latest technological advancements and I look forward to new experiences everyday. </p>
-        <h2>MY CERTIFICATIONS:</h2>
-        
-        <div><Certifications/></div>
         <br></br>
+
+        <div className="buttons">
+        <h2>ACCOLADES</h2>
+          <button onClick={() => handleButtonClick('Merits')}>MERITS AND AWARDS</button>
+          <button onClick={() => handleButtonClick('responsibility')}>POSITIONS OF RESPONSIBILITY</button>
+          <button onClick={() => handleButtonClick('socialwork')}>SOCIAL WORK</button>
+          <button onClick={() => handleButtonClick('languages')}>LANGUAGES KNOWN</button>
+          
+       
+        </div> 
         
         <div><PiFinnTheHumanFill/> DESIGNED BY RAGHA</div>
         </div>
@@ -101,6 +112,58 @@ const HomePage = () => {
             <button className="close-button" onClick={handleClosePopup}>
               &times;
             </button>
+            {activePopup === 'languages' && (
+              <div>
+                <h2>LANGUAGES KNOWS<HiMiniTrophy/></h2>
+                <p>
+                <li>ENGLISH</li>
+                <li>TAMIL</li>
+                <li>HINDI</li>
+                <li>TELUGU</li>
+                <li>FRENCH</li>         
+                </p>
+              </div>
+            )}
+            {activePopup === 'Merits' && (
+              <div>
+                <h2>MERITS AND AWARDS<HiMiniTrophy/></h2>
+                <p>
+                <li>JAN 2022•	IELTS Academic Band 8</li>
+                <li>MAY 2021•	Infosys Toastmasters Fresher’s WFH Representative</li>
+                <li>JAN 2020•	Paper Presentation at International Conference on Contemporary Engineering and technology</li>
+                <li>AUG 2017• Scholarship holder in 1st Semester of B.Tech for obtaining 9.04 CGPA</li>
+                <li>JAN 2015•	Best Student -Inspire Academic Excellence Camp & Certificate of Merit for A1(10/10 CGPA).</li>
+                <li>2010-2020• 1st position in Tamil Speech and Literary Debates over 30 competitions & television shows</li>         
+                </p>
+              </div>
+            )}
+            {activePopup === 'responsibility' && (
+              <div>
+                <h2>POSITIONS OF RESPONSIBILITY<HiMiniTrophy/></h2>
+                <p>
+                <li>NTU Student Invigilator (2022)</li>
+                <li>SRM Classical-Dance-Vice-Captain (2019)</li>
+                <li>Speech committee + Orator (2018-2020)</li>
+                <li>School CCA-Lead (2016)</li>
+                <li>House-Captain (2013, 2014)</li>
+                </p>
+              </div>
+            )}
+            {activePopup === 'socialwork' && (
+              <div>
+                <h2>SOCIAL CONTRIBUTIONS<HiMiniTrophy/></h2>
+                <p>
+                <li>Donation & volunteering - Willing Heart SG </li>
+                <li>Giving.sg (2023-2024)</li>
+                <li>Shimano Beach Cleaning (2023)</li>
+                <li>NTU Safety Poster Designing(2022)</li>
+                <li>Infosys Woman wellbeing group (2020-2022)</li>
+                <li>Tamil valar-mandram-SRM IST (2016- 2021)</li>
+                <li>Scouts and Guides Member(2013-2016)</li>
+                <li>National Service Scheme (2016-2020)</li>
+                </p>
+              </div>
+            )}
             {activePopup === 'devops' && (
               <div>
                 <h2>DevOps- CICD Tools & Technologies</h2>
